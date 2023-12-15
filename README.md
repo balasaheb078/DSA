5a30243e8915b0f63d3d1d4b85b03b86ffac5a10
Balasaheb Hinge <baluhinge076@gmail.com>
2023-12-15 14:56:12 +0530

    Add new line

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DSA
+
+Author - Balasaheb Hinge
---
+++ patterns-printing.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int i;
+    i = 1;
+
+    cout << "value of n:" << endl;
+    cin >> n;
+/* for pattern
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+    1 2 3 4 5
+*/
+    
+
+/*        while(i<=n){
+
+          int  j;
+          j=1;
+            while(j<=n){
+                cout<<j<<" ";
+                j++;
+            };
+            cout<<endl;
+            i=i+1;
+        };
+*/
+    // ============================================
+
+    /* for pattern
+     *
+     **
+     ***
+     ****
+     *****
+     */
+    /*
+       while(i<=n){
+
+         int  j;
+         j=1;
+           while(j<=i){
+               cout<<"*";
+               j++;
+           };
+           cout<<endl;
+           i=i+1;
+       };
+
+    */
+}
