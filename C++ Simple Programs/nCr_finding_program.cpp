c2c4b22197351b9de157339f4e369fef3d9dd137
Balasaheb Hinge <baluhinge076@gmail.com>
2023-12-19 21:45:56 +0530

    more programs added

---
+++ C++ Simple Programs/nCr_finding_program.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact=fact*i;
+
+    }
+    return fact;
+    
+}
+
+int nCr(int n, int r){
+    int numertor= factorial(n);
+
+    int denominator = factorial(r)*factorial(n-r);
+
+    return numertor/denominator;
+
+}
+
+int main()
+{
+    int n,r;
+
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+    cout<<"Enter the value of r:"<<endl;
+    cin>>r;
+
+    cout<<n<<"C"<<r<<"="<<nCr(n,r);
+
+}
---
+++ C++ Simple Programs/simpleArithmaticCalc.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    char temp;
+    char op;
+
+    
+    do
+    {
+
+        cout << "Enter the value of a:" << endl;
+        cin >> a;
+        cout << "Enter the value of b:" << endl;
+        cin >> b;
+        cout << "Enter the Operation you want to perform" << endl;
+        cin >> op;
+
+        switch (op)
+        {
+        case '+':
+            cout << a << "+" << b << "=" << a + b << endl;
+            break;
+        case '-':
+            cout << a << "-" << b << "=" << a - b << endl;
+            break;
+        case '*':
+            cout << a << "*" << b << "=" << a * b << endl;
+            break;
+        case '/':
+            cout << a << "/" << b << "=" << a / b << endl;
+            break;
+
+        default:
+            cout << "Invalid Operations Please try again..." << endl;
+            break;
+        }
+        cout << "Do you want to continue(y Or n)?" << endl;
+        cin >> temp;
+    }
+
+    while (temp != 'n');
+
+    cout << "Thank You !" << endl;
+}
