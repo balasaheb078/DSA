ac6a486086869a20d4a17eb91e1eac44ba7252e2
Balasaheb Hinge <128572867+balasaheb078@users.noreply.github.com>
2023-12-30 20:37:10 +0530

    Add files via upload

---
+++ linear_search.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[],int size, int key){
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]==key)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+    
+}
+
+int main(){
+    int arr[10]={4,-5,66,1,33,75,69,10,0,12};
+    int size;
+    cout<<"Enter the element to be search"<<endl;
+    int key;
+    cin>>key;
+
+    bool found = search(arr,10,key);
+    if (found)
+    {
+        cout<<"Element is present in array."<<endl;
+
+    }
+    else{
+        cout<<"Element is not present in array."<<endl;
+    }
+    
+}
