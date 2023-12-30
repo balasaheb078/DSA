a731a89e37d95540b9110a0e20ed4ce05a5b8a18
Balasaheb Hinge <128572867+balasaheb078@users.noreply.github.com>
2023-12-30 20:39:25 +0530

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,34 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool search(int arr[],int size, int key){
+bool search(int arr[], int size, int key)
+{
     for (int i = 0; i < size; i++)
     {
-        if (arr[i]==key)
+        if (arr[i] == key)
         {
             return 1;
         }
-        
     }
     return 0;
-    
 }
 
-int main(){
-    int arr[10]={4,-5,66,1,33,75,69,10,0,12};
+int main()
+{
+    int arr[10] = {4, -5, 66, 1, 33, 75, 69, 10, 0, 12};
     int size;
-    cout<<"Enter the element to be search"<<endl;
+    cout << "Enter the element to be search" << endl;
     int key;
-    cin>>key;
+    cin >> key;
 
-    bool found = search(arr,10,key);
+    bool found = search(arr, 10, key);
     if (found)
     {
-        cout<<"Element is present in array."<<endl;
-
+        cout << "Element is present in array." << endl;
     }
-    else{
-        cout<<"Element is not present in array."<<endl;
+    else
+    {
+        cout << "Element is not present in array." << endl;
     }
-    
 }
