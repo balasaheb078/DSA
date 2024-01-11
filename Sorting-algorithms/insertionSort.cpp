6397c82369590c1bf6c5ecaf872174c121bc1405
Balasaheb Hinge <128572867+balasaheb078@users.noreply.github.com>
2024-01-11 11:38:35 +0530

    Add files via upload

---
+++ Sorting-algorithms/insertionSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+
+        for (j; j >= 0; j--)
+        {
+            if (temp < arr[j])
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[5] = {4, 6, 55, 10, 8};
+
+    cout << "Elements before sorting:" << endl;
+    printArray(arr, 5);
+    insertionSort(arr, 5);
+    cout << "Elements after sorting:" << endl;
+    printArray(arr, 5);
+}
